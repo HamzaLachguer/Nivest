b2e3066a367ed498a22da604375213850f9e067c
hamzalachguer <lachguerhmza@gmail.com>
2025-09-20 11:10:14 +0200

    Reviews section

--- JS/script.js
+++ JS/script.js
@@ -19,12 +19,17 @@ const DOM_ELEMENTS = {
   hours: getElement("#hours"),
   minutes: getElement("#minutes"),
   seconds: getElement("#seconds"),
+
+  slideTogglers: getElement("#slide-toggle"),
+  slideContent: getElement("#slide-content"),
 }
 
 let stats = {
   favoritesList: new Set(),
 
   targetDate: new Date("November 1 2025 00:00:00").getTime(),
+
+  slideIndex: 0,
 }
 
 
@@ -192,7 +197,78 @@ function counter() {
 setInterval(counter, 1000)
 
 
+/* Testimenials */
+//
+const testimenials = [
+  {
+    review: "These aren’t just sneakers. They’re motivation. I put them on and I move — simple as that. They feel built for momentum, not just comfort. Every step feels like purpose.",
+    name: "Marcus Petrov",
+    country: "Netherlands",
+    imgSrc: "https://framerusercontent.com/images/utC4J2FwNnDujUCNIcD0nEcHNw.png?scale-down-to=512"
+  },  {
+    review: "Finally found kicks that don’t just look sharp — they actually keep up. On the court, in the city, wherever. I’ve put them through heat, rain, and long days. Never missed a beat in entire life.",
+    name: "Alina Reyes",
+    country: "Spain",
+    imgSrc: "https://framerusercontent.com/images/696Q9tY9SbzS5TpEg234BbCxXVQ.png?scale-down-to=512"
+  },  {
+    review: "Comfort? Check. Grip? Solid. Style? You already know. I’ve worn them daily and they still feel brand new. These shoes aren’t just part of my outfit — they’re part of my rhythm.",
+    name: "Jamal Okoro",
+    country: "England",
+    imgSrc: "https://framerusercontent.com/images/aLQJz65d0ZahdyQjRJ7fH3ogNKY.png?scale-down-to=512"
+  }
+]
 
+// update slider
+function updateSlider(index) {
+  const testimenial = testimenials[index];
+  const {review, name, country, imgSrc} = testimenial;
+
+  return DOM_ELEMENTS.slideContent.innerHTML = `
+  <div class="flex flex-col gap-6 items-center">
+    <div>
+      <svg fill="#D2FF1F" height="16" width="18" id="svg-453449676_583" viewBox="0 0 24 20"><path d="M0 20v-6.694c0-1.653.161-3.223.483-4.711a17.84 17.84 0 0 1 1.772-4.38C3.115 2.782 4.215 1.377 5.557 0l3.624 2.893C8.161 3.994 7.33 5.04 6.685 6.033a11.92 11.92 0 0 0-1.289 2.975 13.282 13.282 0 0 0-.322 2.976l-2.255-1.819h6.765V20H0Zm14.416 0v-6.694c0-1.653.161-3.223.483-4.711a17.845 17.845 0 0 1 1.772-4.38c.86-1.433 1.96-2.838 3.302-4.215l3.624 2.893c-1.02 1.101-1.852 2.148-2.496 3.14-.59.992-1.02 1.984-1.289 2.975a13.28 13.28 0 0 0-.322 2.976l-2.255-1.819H24V20h-9.584Z" fill="#D2FF1F"></path></svg>
+    </div>
+
+    <p class="text-center text-2xl lg:text-4xl uppercase font-semibold text-white leading-none">${review}</p>
+  </div>
+
+  <div class="flex flex-col gap-6 items-center">
+    <div class="overflow-hidden h-14 w-14 rounded-full">
+      <img class="h-full w-full object-cover object-center" src=${imgSrc} alt="" loading="lazy">
+    </div>
+
+    <div class="flex flex-col items-center gap-1">
+      <span class="text-sm text-white capitalize">${name}</span>
+      <span class="text-sm text-white-99 capitalize">Customer from ${country}</span>
+    </div>
+  </div>
+  `
+}
+
+updateSlider(stats.slideIndex)
+
+// generate togglers
+testimenials.forEach((T, index) => {
+  const btn = document.createElement("button");
+  btn.setAttribute("data-slide-index", index);
+  btn.className = "bg-white-99 h-1 w-10 lg:w-40 transition-150";
+
+  if (index === stats.slideIndex) {
+    btn.className = "bg-white h-1 w-20 lg:w-40 transition-150";
+  }
+  DOM_ELEMENTS.slideTogglers.append(btn);
+  btn.addEventListener('click', () => {
+    Array.from(DOM_ELEMENTS.slideTogglers.querySelectorAll("button")).forEach(b => {
+      b.className = "bg-white-99 h-1 w-10 lg:w-40 transition-150";
+    })
+
+    btn.className = "bg-white h-1 w-20 lg:w-40 transition-150";
+    const slideIndex = btn.dataset.slideIndex;
+    stats.slideIndex = slideIndex;
+
+    updateSlider(stats.slideIndex);
+  })
+})
 
 
 
