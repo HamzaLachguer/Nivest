4741f4c29d88e07dc76e91047ce15794c3e195b6
hamzalachguer <lachguerhmza@gmail.com>
2025-10-05 13:33:06 +0200

    Filtering functionalities

--- JS/shop-page.js
+++ JS/shop-page.js
@@ -21,12 +21,16 @@ const DOM_ELEMENTS = {
 
   filterHeader: getElement("#filter-header"),
   filterOptions: getElement("#filter-options"),
+
+  filterPrice: getElement("#filter-by-price"),
+  filterCollection: getElement("#filter-by-collection"),
+  filterGender: getElement("#filter-by-gender"),
 }
 
 
 let largeScreen = 1024;
 let filterOptionOpen = true;
-let filterBy = new Set();
+// let filterBy = new Set();
 
 DOM_ELEMENTS.filterHeader.addEventListener('click', () => {
   if (window.innerWidth >= largeScreen) return
@@ -54,37 +58,123 @@ function hideFilterOptions() {
 }
 
 
-function toogleFilterId(id) {
-  filterBy.has(id) ? filterBy.remove(id) : filterBy.add(id);
+// function toogleFilterId(id) {
+//   filterBy.has(id) ? filterBy.remove(id) : filterBy.add(id);
+// }
+
+// let filterdList = null;
+
+
+
+
+const getGridData = async () => {
+  const data = await loadData(URL);
+  return data;
 }
 
-let filterdList = null;
+let allData = await getGridData();
+let data = [...allData];
+
+let filterData = {
+  categories: new Set(),
+  tags: new Set(),
+};
+
 DOM_ELEMENTS.filterOptions.addEventListener('click', (e) => {
   const option = e.target.closest(".filter-btn");
   if (!option) return;
 
-  const filterId = option.dataset.filter;
-  toogleFilterId(filterId);
+  option.querySelector("div:nth-of-type(1)").classList.toggle("active-filter");
+  const filterGender = option.dataset.category;
+  const filterCollection = option.dataset.tag;
+  
+  if (filterGender){
+    if (filterGender === "all") {
+      filterData.categories.clear();
+      return data = [...allData];
+    } 
 
-  //filterdList.filter()
-  console.log(filterBy);
+    filterData.categories.has(filterGender)
+      ? filterData.categories.delete(filterGender)
+      : filterData.categories.add(filterGender);
+  }
+
+  if (filterCollection){
+    filterData.tags.has(filterCollection)
+    ? filterData.tags.delete(filterCollection) 
+    : filterData.tags.add(filterCollection);
+  }
+
+  data = allData.filter(p => {
+    const matchesCategories = filterData.categories.size === 0 || filterData.categories.has(p.category);
+    const matchesTags = filterData.tags.size === 0 || filterData.tags.has(p.tag);
+    return matchesCategories && matchesTags;
+  })
+
+  getElement("#filter-product-grid").innerHTML = "";
+  renderProductGrid(data, "#filter-product-grid", data.length);
 })
 
+// async function initFilter() {
+//   let filterGender = new Set();
+//   let filterCollection = new Set();
+
+//   DOM_ELEMENTS.filterGender.addEventListener('click', async (e) => {
+//     const option = e.target.closest(".filter-btn");
+//     if (!option) return;
+    
+//     option.querySelector("div:nth-of-type(1)").classList.toggle("active-filter");
+//     const filterId = option.dataset.category;
+
+//     if (filterId === "all") {
+//       filterGender.clear();
+//       data = [...allData];
+//       return;
+//     }
+
+//     filterGender.has(filterId) ? filterGender.delete(filterId) : filterGender.add(filterId);
+
+//     if (filterGender.size === 0) {
+//       data = [...allData];
+//     } else {
+//       data = allData.filter(p => filterGender.has(p.category));
+//     }
+
+//     getElement("#filter-product-grid").innerHTML = "";
+//     renderProductGrid(data, "#filter-product-grid", data.length);
+//   })
+
+//   DOM_ELEMENTS.filterCollection.addEventListener('click', async (e) => {
+//     const option = e.target.closest(".filter-btn");
+//     if (!option) return;
+    
+//     option.querySelector("div:nth-of-type(1)").classList.toggle("active-filter");
+//     const filterId = option.dataset.tag;
+
+//     filterCollection.has(filterId) ? filterCollection.delete(filterId) : filterCollection.add(filterId);
+//     if (filterCollection.size === 0) {
+//       data = [...allData];
+//     } else {
+//       data = allData.filter(p => filterCollection.has(p.tag));
+//     }
+
+//     getElement("#filter-product-grid").innerHTML = "";
+//     renderProductGrid(data, "#filter-product-grid", data.length);
+//   })
+
+// }
+
+
+//initFilter();
 /* ============================== */
 // Product grid
 //
 /* ============================== */
 
 
-const getGridData = async (...filterBy) => {
 
-  const data = await loadData(URL);
-  return data;
-}
 
-let data = await getGridData();
-
-renderProductGrid(data, "#filter-product-grid", data.length)
+renderProductGrid(data, "#filter-product-grid", data.length);
 
 
 hidePopup();
